461daab96718959fb83a733d117b949e9da7e4a0
ayocodingit <ayocodingit@gmail.com>
2021-04-25 11:30:00 +0700

    add dockerfile

---
+++ docker-config/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+app=${DOCKER_APP:-app}
+
+if [ "$app" = "app" ]; then
+
+    echo "Running the app..."
+    ## Run Optimization
+    php /var/www/html/artisan optimize
+    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+elif [ "$app" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan queue:work --queue=default --sleep=3 --tries=3 --timeout=90
+
+elif [ "$app" = "scheduler" ]; then
+
+    echo "Running the scheduler..."
+    while [ true ]
+    do
+        php /var/www/html/artisan scheduler:run --no-interaction &
+        sleep 60
+    done
+
+else
+    echo "Could not match the container app \"$app\""
+    exit 1
+fi
