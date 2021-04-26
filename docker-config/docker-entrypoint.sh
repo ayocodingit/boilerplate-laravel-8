860d5e5e2db04c7c6fc85c9b9cc13bfd60c63716
ayocodingit <ayocodingit@gmail.com>
2021-04-26 22:09:52 +0700

    update laravel octane

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 app=${DOCKER_APP:-app}
+worker=${OCTANE_WORKER:-1}
 
 if [ "$app" = "app" ]; then
 
@@ -11,6 +12,11 @@ elif [ "$app" = "queue" ]; then
 
     echo "Running the queue..."
     php /var/www/html/artisan queue:work --queue=default --sleep=3 --tries=3 --timeout=90
+
+elif [ "$app" = "octane" ]; then
+
+    echo "Running the app with octane count worker $worker..."
+    php /var/www/html/artisan octane:start --host=0.0.0.0 --port=8080 --workers=$worker -n
 
 elif [ "$app" = "scheduler" ]; then
 
